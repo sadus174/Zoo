c94010ff30fc17f804f81d6ca4b7722c3765a37b
Криворотов Павел Васильевич <p.krivorotov@dtst.su>
2025-03-29 10:18:04 +0500

    Авторизация

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(294, 275);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Войти";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(424, 275);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Отмена";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(329, 88);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(331, 134);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Зоопарк - Подажа билетов";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,53 @@
+
+using MySql.Data.MySqlClient;
+
 namespace Zoo
 {
     public partial class Form1 : Form
     {
+        string connStr = "server=10.80.1.98;port=3306;user=zoo;database=lik;password=293fh290fg9(#9fh";
+        MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new MySqlConnection(connStr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = textBox1.Text;
+            string pswd = textBox2.Text;
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            string sql = "SELECT COUNT(id) as 'Количество пользователей' FROM T_Users WHERE login='"+login+ "' and pswd='"+ pswd + "' and activ=1";
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            // выполняем запрос и получаем ответ
+            int count = Convert.ToInt32(command.ExecuteScalar().ToString());
+            // выводим ответ в консоль
+
+            if (count > 0)
+            {
+                MessageBox.Show("Учётные данные верны");
+            }
+            else
+            {
+                MessageBox.Show("Ошибка учётных данных");
+            }
+
+            // закрываем соединение с БД
+            conn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Close();
         }
     }
 }
